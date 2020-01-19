47a833396009bedc791ade23dc5609a3fef5a385
Christian Schuller <cschuller@servusalps.com>
2020-01-19 15:38:23 +0100

    Rename folder lib to src

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+import {Zitrusmix} from './Zitrusmix';
+import {ContentElement} from './ContentElement';
+import {PluginContext} from './plugin/PluginContext';
+
+import {logTable} from './plugins/logTable';
+
+export {
+    Zitrusmix,
+    ContentElement,
+
+    PluginContext,
+
+    logTable
+};
